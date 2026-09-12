405183eea151dc0d4f8f71df64e8d9d33e0b16a7
Alex Galochkin <alexgalochkin7@yandex.ru>
2026-09-12 15:22:54 +0300

    add Function classes

---
+++ src/main/java/ru/yandex/practicum/sleeptracker/functionalInterfaces/AverageDurationFunction.java
@@ -0,0 +1,18 @@
+package ru.yandex.practicum.sleeptracker.functionalInterfaces;
+
+import ru.yandex.practicum.sleeptracker.data.SleepAnalysisResult;
+import ru.yandex.practicum.sleeptracker.data.SleepingSession;
+
+import java.util.List;
+
+public class AverageDurationFunction implements SleepAnalysisFunction {
+    @Override
+    public SleepAnalysisResult<Double> apply(List<SleepingSession> sessions) {
+        double avg = sessions.stream()
+                .mapToLong(SleepingSession::getDurationOnMinutes)
+                .average()
+                .orElse(0.0);
+        return new SleepAnalysisResult<>("Средняя продолжительность сессии (в минутах)",
+                Math.round(avg * 100.0) / 100.0);
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/sleeptracker/functionalInterfaces/BadQualitySessionsFunction.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.sleeptracker.functionalInterfaces;
+
+import ru.yandex.practicum.sleeptracker.data.SleepAnalysisResult;
+import ru.yandex.practicum.sleeptracker.data.SleepingSession;
+
+import java.util.List;
+import static ru.yandex.practicum.sleeptracker.data.SleepQuality.BAD;
+
+public class BadQualitySessionsFunction implements SleepAnalysisFunction {
+    @Override
+    public SleepAnalysisResult<Long> apply(List<SleepingSession> sessions) {
+        long badCount = sessions.stream()
+                .filter(s -> s.getQuality() == BAD)
+                .count();
+        return new SleepAnalysisResult<>("Количество сессий с плохим качеством сна", badCount);
+    }
+}
